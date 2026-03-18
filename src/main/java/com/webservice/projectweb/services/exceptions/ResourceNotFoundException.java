df9d15d63b8d118def0f8fe38f6ee2c137f3cc73
henrryribeiro <henrry.marcell@gmail.com>
2026-03-18 11:45:50 -0300

    Exception handling - delete

--- src/main/java/com/webservice/projectweb/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/webservice/projectweb/services/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.webservice.projectweb.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Object id) {
         super("Resource not found. Id" + id);
